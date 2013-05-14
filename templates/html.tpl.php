8ab7eb1a9c67b08c8ab00ae3c63bb35024b7885b
Anton Staroverov <stant.ru@gmail.com>
2013-05-14 10:51:50 +0400

    Improved compatibility with IE6-8 (via html5shiv). Also added Google Chrome Frame.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -8,17 +8,25 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="<?php print $language->language ?>">
-<head profile="<?php print $grddl_profile ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php print $head ?>
-  <title><?php print $head_title ?></title>
-  <?php print $styles ?>
-  <?php print $scripts ?>
-</head>
-<body class="<?php print $classes ?>"<?php print $attributes ?>>
-  <?php print $page_top ?>
-  <?php print $page ?>
-  <?php print $page_bottom ?>
-</body>
+<html lang="<?php print $language->language ?>" class="no-js">
+  <head profile="<?php print $grddl_profile ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php print $head ?>
+    <title><?php print $head_title ?></title>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+    <!-- HTML5 support for IE6-8 -->
+    <!--[if lt IE 9]>
+      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body class="<?php print $classes ?>"<?php print $attributes ?>>
+    <?php print $page_top ?>
+    <!--[if lt IE 7]>
+      <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+    <?php print $page ?>
+    <?php print $page_bottom ?>
+  </body>
 </html>
