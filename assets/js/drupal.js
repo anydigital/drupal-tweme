2e097d20a9b0625162c128783dee874fe437f567
Anton Staroverov <stant.ru@gmail.com>
2013-05-26 13:47:22 +0400

    Automatic styles for tables.

---
+++ assets/js/drupal.js
@@ -0,0 +1,6 @@
+(function ($) {
+  $(document).ready(function() {
+    $('.field-type-text-long table').addClass('table table-bordered');
+    $('.field-type-text-with-summary table').addClass('table table-bordered');
+  });
+})(jQuery);
