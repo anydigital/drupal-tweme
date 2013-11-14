15ad61c9ca1eede517a027ba62ab588aab3ef20d
Anton Staroverov <inbox@tonystar.ru>
2013-11-14 14:35:48 -0800

    Better layout organization

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -7,106 +7,102 @@
 
 ?>
 
-<!-- Navbar -->
-<div id="navbar" class="navbar navbar-medium navbar-inverse navbar-static-top">
-	<div class="navbar-inner">
-		<div class="container">
+<!-- NAVBAR -->
+<div class="id-navbar navbar navbar-medium navbar-inverse navbar-static-top">
+  <div class="navbar-inner">
+    <div class="container">
       <?php print $navbar_toggler ?>
-			<?php print $navbar_brand ?>
+      <?php print $navbar_brand ?>
       <?php print $navbar_search ?>
       <?php if ($navbar_menu): ?>
-			<nav class="nav-collapse collapse" role="navigation">
+      <nav class="nav-collapse collapse" role="navigation">
         <?php print $navbar_menu ?>
       </nav>
-			<?php endif ?>
-		</div>
-	</div>
-</div>
-
-<?php if ($has_header): ?>
-<!-- Header -->
-<div id="header" class="zone container-wrapper hidden-phone">
-  <div class="container">
-    <div class="inner">
-      <div id="header-content" class="mkt">
-        <?php print $headline ?>
-        <?php print render($page['featured']) ?>
-        <?php print render($page['header']) ?>
-      </div>
-      <?php print $controls ?>
+      <?php endif ?>
     </div>
   </div>
 </div>
+
+<?php if ($page['featured']): ?>
+<!-- FEATURED -->
+<div class="zone zone-featured container-wrapper">
+  <?php print render($page['featured']) ?>
+</div>
 <?php endif ?>
 
-<?php if ($has_highlights): ?>
-<!-- Highlighted -->
-<header id="highlighted" class="zone container-wrapper">
+<?php if ($has_header): ?>
+<!-- HEADER -->
+<header class="zone zone-header container-wrapper">
   <div class="container">
-    <div class="inner mkt size-l align-center">
+    <div class="zone-inner">
+      <?php print $headline ?>
+      <?php print $messages ?>
+      <?php print render($page['header']) ?>
+      <?php print render($page['help']) ?>
+      <?php print $controls ?>
+    </div>
+  </div>
+</header>
+<?php endif ?>
+
+<?php if ($page['highlighted']): ?>
+<!-- HIGHLIGHTED -->
+<header class="zone zone-highlighted zone-annex container-wrapper">
+  <div class="container">
+    <div class="zone-inner mkt size-l align-center">
       <?php print render($page['highlighted']) ?>
     </div>
   </div>
 </header>
 <?php endif ?>
 
-<!-- Main -->
-<div id="main" class="zone container-wrapper">
+<!-- MAIN -->
+<div class="zone zone-main container-wrapper">
   <div class="container">
-    <div class="zone-main-inner inner">
-      <?php print render($page['help']) ?>
-      <?php print $messages ?>
+    <div class="zone-inner id-pin-to">
       <div class="row-fluid">
         <?php if ($has_sidebar_first): ?>
-        <!-- Sidebar first -->
-        <aside id="sidebar-first" class="sidebar span3 hidden-phone">
-          <div class="inner">
+        <!-- SIDEBAR FIRST -->
+        <aside class="span3">
+          <div class="sidebar sidebar-first">
             <?php print render($page['sidebar_first']) ?>
-            <?php if ($page['sidebar_first_affix']): ?>
-              <div data-spy="pin" data-container=".zone-main-inner">
-                <?php print render($page['sidebar_first_affix']) ?>
-              </div>
-            <?php endif ?>
+            <?php print render($page['sidebar_first_affix']) ?>
           </div>
         </aside>
         <?php endif ?>
-        <!-- Content -->
-        <section id="content" class="span<?php print 12 - 3 * $has_sidebar_first - 3 * $has_sidebar_second ?>">
+        <!-- CONTENT -->
+        <section class="span<?php print 12 - 3 * $has_sidebar_first - 3 * $has_sidebar_second ?>">
           <?php print render($page['content']) ?>
         </section>
         <?php if ($has_sidebar_second): ?>
-        <!-- Sidebar second -->
-        <aside id="sidebar-second" class="sidebar span3 hidden-phone">
-          <div class="inner">
+        <!-- SIDEBAR SECOND -->
+        <aside class="span3">
+          <div class="sidebar sidebar-second">
             <?php print render($page['sidebar_second']) ?>
-            <?php if ($page['sidebar_second_affix']): ?>
-              <div data-spy="pin" data-container=".zone-main-inner">
-                <?php print render($page['sidebar_second_affix']) ?>
-              </div>
-            <?php endif ?>
+            <?php print render($page['sidebar_second_affix']) ?>
           </div>
         </aside>
         <?php endif ?>
       </div>
     </div>
-	</div>
+  </div>
 </div>
 
 <?php if ($page['bottom']): ?>
-<!-- Bottom -->
-<header id="bottom" class="zone container-wrapper">
+<!-- BOTTOM -->
+<header class="zone zone-bottom zone-annex container-wrapper">
   <div class="container">
-    <div class="inner">
+    <div class="zone-inner">
       <?php print render($page['bottom']) ?>
     </div>
   </div>
 </header>
 <?php endif ?>
 
-<!-- Footer -->
-<footer id="footer" class="zone container-wrapper">
-	<div class="container">
-    <div class="inner size-s align-center">
+<!-- FOOTER -->
+<footer class="zone zone-footer container-wrapper">
+  <div class="container">
+    <div class="zone-inner size-s align-center">
       <?php print $page['footer'] ? render($page['footer']) : $copyright ?>
       <?php if ($secondary_menu): ?>
       <div class="footer-links">
@@ -119,5 +115,5 @@
       <div class="footer-icons"><?php print $feed_icons ?></div>
       <?php endif ?>
     </div>
-	</div>
+  </div>
 </footer>
--- templates/region--featured.tpl.php
+++ templates/region--featured.tpl.php
@@ -7,13 +7,11 @@
 
 ?>
 <?php if ($content): ?>
-<div id="featured" class="carousel slide">
-  <!-- Carousel items -->
+<div class="id-featured carousel slide">
   <div class="carousel-inner">
     <?php print $content ?>
   </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control carousel-control-modern left" href="#featured" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control carousel-control-modern right" href="#featured" data-slide="next">&rsaquo;</a>
+  <a class="carousel-control carousel-control-modern left" href=".id-featured" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control carousel-control-modern right" href=".id-featured" data-slide="next">&rsaquo;</a>
 </div>
 <?php endif ?>
