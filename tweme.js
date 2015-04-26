1298f30d110dc61c0057a572ed7be559d3536563
Anton Staroverov <by@tonystar.me>
2015-04-26 03:32:17 +0300

    Add basic footer sitemap style

---
+++ tweme.js
@@ -0,0 +1,7 @@
+(function($) {
+  $(function () {
+    $('.footer .menu > li').addClass('col-sm-6 col-md-4 col-lg-2');
+    $('.footer .menu').removeClass().addClass('sitemap list-unstyled row');
+    $('.footer .dropdown-menu').removeClass().addClass('nav');
+  });
+})(jQuery);
