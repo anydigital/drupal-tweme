a2c1f6a7c15ec2f6fe51bc8e83f481eca8ea7f03
Anton Staroverov <by@tonystar.me>
2015-05-01 14:09:40 +0300

    Show navbar dropdowns on hover

--- tweme.js
+++ tweme.js
@@ -1,5 +1,8 @@
 (function($) {
   $(function () {
+    $('.navbar-nav .dropdown-toggle').on('click', function () {
+      window.location.href = $(this).attr('href');
+    });
     $('.footer.has-sitemap .menu > li').addClass('col-sm-6 col-md-4 col-lg-2');
     $('.footer.has-sitemap .menu').removeClass().addClass('footer-sitemap list-unstyled row');
     $('.footer.has-sitemap .dropdown-menu').removeClass().addClass('nav');
