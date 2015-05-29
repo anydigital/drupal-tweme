2161ee7857998e61ac76a96c163867df339c3f8d
Anton Staroverov <by@tonystar.me>
2015-05-29 19:34:56 +0300

    Book navigation block very basic

--- template.php
+++ template.php
@@ -80,3 +80,68 @@ function tweme_menu_link__menu_footer_sitemap($vars) {
   }
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
+
+function tweme_preprocess_menu_link(&$vars) {
+  $element = &$vars['element'];
+
+  if ($element['#original_link']['module'] == 'book') {
+    if (!empty($element['#below']) && is_array($element['#below'])) {
+      $element['#below']['#theme_wrappers'] = array('menu_tree__book_toc');
+      foreach (element_children($element['#below']) as $key) {
+        $sub_element = $element['#below'][$key];
+        if (!empty($sub_element['#below']) && is_array($sub_element['#below'])) {
+          $element['#below']['#suffix'] = drupal_render($sub_element);
+          $element['#below'][$key]['#below'] = array();
+        }
+      }
+    }
+  }
+}
+
+function tweme_menu_link($vars) {
+  $element = $vars['element'];
+
+  if ($element['#original_link']['module'] == 'book') {
+    return tweme_menu_link__book_toc($vars);
+  }
+  return bootstrap_menu_link($vars);
+}
+
+function tweme_preprocess_book_all_books_block(&$vars) {
+  $element = array(
+    '#theme' => 'menu_link__book_toc',
+    '#original_link' => array(
+      'module' => 'book',
+    ),
+    '#below' => array(),
+  );
+  foreach ($vars['book_menus'] as $menu) {
+    $element['#below'] += $menu;
+  }
+
+  $vars['book_menus'] = array(
+    array(
+      '#prefix' => '<div class="row">',
+      '#suffix' => '</div>',
+      'element' => $element,
+    ),
+  );
+}
+
+function tweme_menu_tree__book_toc($vars) {
+  return '<div class="col-md-4"><ul class="list-unstyled">' . $vars['tree'] . '</ul></div>';
+}
+
+function tweme_menu_link__book_toc($vars) {
+  $element = $vars['element'];
+
+  if (!empty($element['#below'])) {
+    return drupal_render($element['#below']);
+  }
+  return theme_menu_link($vars);
+}
+
+function tweme_preprocess_book_navigation(&$vars) {
+  $vars['tree'] = '';
+  $vars['has_links'] = FALSE;
+}
