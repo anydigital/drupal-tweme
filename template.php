87e75fbdf68261c92e3ee1951df60c33e07c9b28
Ng Chin Kiong <ckng@myfinejob.com>
2015-11-02 18:06:18 +0800

    fixed Fatal error: [] operator not supported for strings 2

--- template.php
+++ template.php
@@ -127,6 +127,9 @@ function tweme_preprocess_poll_bar(&$vars) {
  * Preprocesses variables for theme_item_list().
  */
 function tweme_preprocess_item_list(&$vars) {
+  if (!isset($vars['attributes']['class'])) {
+    $vars['attributes']['class'] = array();
+  }
   if (!is_array($vars['attributes']['class'])) {
     $vars['attributes']['class'] = array($vars['attributes']['class']);
   }
